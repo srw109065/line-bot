817b6b6b60d6660bf2d4e7ebb0be6148d15fa026
Dio <srw109065@gmail.com>
2022-05-02 12:04:17 +0800

    Line更新1.2

--- app.py
+++ app.py
@@ -40,10 +40,14 @@ def handle_message(event):
     msg = event.message.text
     r = "很抱歉，我看不懂你說甚麼"
 
-    if msg == "hi":
+    if msg in ["hi", "HI", "hI", "Hi"]:
         r = "hi"
     elif msg == "你吃飯了嗎":
         r = "還沒"
+    elif msg == "你是誰":
+        r = "我是機器人"
+    elif "訂位" in msg:
+        r = "你想訂位，是嘛?"
 
     line_bot_api.reply_message(
         event.reply_token,
